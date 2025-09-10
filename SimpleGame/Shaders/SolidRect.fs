093cc12d71974087252e26d29ce8d94ad9a0a219
niche0905 <yudle537@gmail.com>
2025-09-10 16:41:23 +0900

    triangle clipping

--- SimpleGame/Shaders/SolidRect.fs
+++ SimpleGame/Shaders/SolidRect.fs
@@ -8,5 +8,9 @@ uniform vec4 u_Color;
 void main()
 {
 	//FragColor = vec4(u_Color.r, u_Color.g, u_Color.b, u_Color.a);
-	FragColor = v_Color;
+	if (v_Color.b < 0.5 && v_Color.r < 0.5)
+		FragColor = v_Color;
+	else
+		discard;	// predication이 안되는 GPU에서는 성능 저하가 있을 수 있음
+		
 }
