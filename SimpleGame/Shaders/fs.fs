9660bee517020efc2add8a427ac4cd77ec2347ee
niche0905 <yudle537@gmail.com>
2025-11-11 15:09:50 +0900

    wave line fs prof.ver

--- SimpleGame/Shaders/fs.fs
+++ SimpleGame/Shaders/fs.fs
@@ -5,8 +5,14 @@ layout(location=0) out vec4 FragColor;
 in vec2 v_UV;
 
 
+const float c_PI = 3.141592;
+
 void main()
 {
-	vec4 newColor = vec4(sin(2 * 3.14 * v_UV.x * 4));
+	float vx = 2 * c_PI * v_UV.x * 8;	// 0 ~ 2pi
+	float vy = 2 * c_PI * v_UV.y * 8;	// 0 ~ 2pi
+	float grayScale = sin(vy);
+	
+	vec4 newColor = vec4(grayScale);
 	FragColor = newColor;
 }
