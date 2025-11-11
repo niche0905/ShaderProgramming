f67bb43474209873a0d85ffbf8163ebdf44598b4
niche0905 <yudle537@gmail.com>
2025-11-11 15:13:51 +0900

    snake scales fs prof.ver

--- SimpleGame/Shaders/fs.fs
+++ SimpleGame/Shaders/fs.fs
@@ -11,7 +11,7 @@ void main()
 {
 	float vx = 2 * c_PI * v_UV.x * 8;	// 0 ~ 2pi
 	float vy = 2 * c_PI * v_UV.y * 8;	// 0 ~ 2pi
-	float grayScale = sin(vy);
+	float grayScale = pow(abs(sin(vx) + sin(vy)), 0.5);
 	
 	vec4 newColor = vec4(grayScale);
 	FragColor = newColor;
