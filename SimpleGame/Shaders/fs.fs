d7c428cdf2dd8fd661e9fb4872e251db51b667cd
niche0905 <yudle537@gmail.com>
2025-11-12 16:49:34 +0900

    texture sin wave moving

--- SimpleGame/Shaders/fs.fs
+++ SimpleGame/Shaders/fs.fs
@@ -7,12 +7,15 @@ uniform sampler2D u_RGBTexture;
 in vec2 v_UV;
 uniform float u_Time;
 
+const float c_PI = 3.14159265359;
 
 void main()
 {
     float s = 0.01;
+    vec2 newPos = v_UV;
+    newPos += vec2(0, 0.2 * sin(2 * c_PI * v_UV.x + u_Time));
 
-    vec4 newColor = texture(u_RGBTexture, vec2(v_UV.x + 0.05 * sin(v_UV.y * 2 * 3.141592), v_UV.y));
+    vec4 newColor = texture(u_RGBTexture, newPos);
 
     FragColor = newColor;
     //FragColor = vec4(v_UV, 0, 1.0);
