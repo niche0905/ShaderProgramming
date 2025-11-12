cf21ce4a4e658ff7eaef24203ea3008f2ace4afb
niche0905 <yudle537@gmail.com>
2025-11-12 16:28:46 +0900

    blur effect

--- SimpleGame/Shaders/fs.fs
+++ SimpleGame/Shaders/fs.fs
@@ -12,6 +12,14 @@ void main()
 {
     vec4 newColor = texture(u_RGBTexture, v_UV);
 
+    float s = 0.01;
+    newColor += texture(u_RGBTexture, vec2(v_UV.x - s * 1, v_UV.y));
+    newColor += texture(u_RGBTexture, vec2(v_UV.x - s * 2, v_UV.y));
+    newColor += texture(u_RGBTexture, vec2(v_UV.x - s * 3, v_UV.y));
+    newColor += texture(u_RGBTexture, vec2(v_UV.x - s * 4, v_UV.y));
+    newColor += texture(u_RGBTexture, vec2(v_UV.x + s * 1, v_UV.y));
+    newColor /= 5;
+
     FragColor = newColor;
     //FragColor = vec4(v_UV, 0, 1.0);
 }
