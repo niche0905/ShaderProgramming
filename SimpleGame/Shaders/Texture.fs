c4e0f530a7bc8312e18cb3357267e23508787356
niche0905 <yudle537@gmail.com>
2025-11-26 16:23:25 +0900

    gaussian blur add

--- SimpleGame/Shaders/Texture.fs
+++ SimpleGame/Shaders/Texture.fs
@@ -3,12 +3,27 @@
 layout(location=0) out vec4 FragColor;
 
 uniform sampler2D u_TexID;
+const vec2 u_TexelSize = vec2(1/512, 1/512);
 
 in vec2 v_Tex;
 
-
 void main()
 {
-	//FragColor = vec4(v_Tex, 0, 1);
-	FragColor = texture(u_TexID, vec2(v_Tex.x, 1 - v_Tex.y));
+    vec2 tex = vec2(v_Tex.x, 1.0 - v_Tex.y);
+
+    vec3 col = vec3(0.0);
+
+    // 3x3 blur kernel
+    for(int x = -1; x <= 1; x++)
+    {
+        for(int y = -1; y <= 1; y++)
+        {
+            vec2 offset = vec2(x, y) * u_TexelSize;
+            col += texture(u_TexID, tex + offset).rgb;
+        }
+    }
+
+    col /= 9.0;
+
+    FragColor = vec4(col, 1.0);
 }
